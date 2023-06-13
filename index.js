62a016a6fc5bd23f967bf4c2cd690958ec003478
TristanM225 <TristanM225@gmail.com>
2023-06-12 23:15:40 -0700

    cleaned up code and centered text

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); // including inquirer
 const fs = require('fs');
 
 const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
-const generateSVG = ({ cx, cy, r, shape, color, letters }) => {
+const generateSVG = ({ shape, color, letters, letterColor }) => {
+  //if in the prompt we pick circle it will render circle here
   if (shape === 'Circle') {
-    const circle = new Circle(cx, cy, r);
+    const circle = new Circle();
     circle.setColor(color);
     circle.setLetters(letters);
+    circle.setLetterColor(letterColor);
     return circle.render();
+    //if in the prompt we pick square it will render square here
   } else if (shape === 'Square') {
-    const square = new Square(cx, cy, r);
+    const square = new Square();
     square.setColor(color);
     square.setLetters(letters);
+    square.setLetterColor(letterColor);
     return square.render();
+    //if in the prompt we pick Triangle it will render Triangle here
   } else if (shape === 'Triangle') {
-    const triangle = new Triangle(cx, cy, r);
+    const triangle = new Triangle();
     triangle.setColor(color);
     triangle.setLetters(letters);
+    triangle.setLetterColor(letterColor);
     return triangle.render();
   }
 };
 
 inquirer
   .prompt([
+    {
+      type: 'input',
+      name: 'letterColor',
+      message: 'Enter text color',
+    },
     {
       type: 'input',
       name: 'letters',
@@ -38,27 +49,13 @@ inquirer
     {
       type: 'input',
       name: 'color',
-      message: 'Choose a color (any!)',
+      message: 'Choose a color',
     },
-    {
-      type: 'input',
-      name: 'cx',
-      message: 'Enter the cx value',
-    },
-    {
-      type: 'input',
-      name: 'cy',
-      message: 'Enter the cy value',
-    },
-    {
-      type: 'input',
-      name: 'r',
-      message: 'Enter the r value',
-    },
+
   ])
   .then((answers) => {
     const svgImageContent = generateSVG(answers);
-
+// takes the answers and makes them into a SVG image file.
     fs.writeFile('logo.svg', svgImageContent, (err) =>
       err ? console.log(err) : console.log('Generated logo.svg')
     );
