8f452282a533a779bf938fd2af68d91c72ca4fdd
TristanM225 <TristanM225@gmail.com>
2023-06-12 22:27:28 -0700

    corrected code in shapes.js n added shape logic in index.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SVG-Generator
-A SVG Generator based on user input
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+
+  <circle cx="undefined" cy="undefined" r="undefined" fill="Green"/>
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">abc</text>
+
+</svg>
--- index.js
+++ index.js
@@ -1,32 +1,65 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
-const myCircle = new Circle(0, 0, 5);
-const mySquare = new Square(0, 0, 10);
-const myTriangle = new Triangle(0, 0, 8);
-
-console.log(myCircle);
-console.log(mySquare);
-console.log(myTriangle);
+const generateSVG = ({ cx, cy, r, shape, color, letters }) => {
+  if (shape === 'Circle') {
+    const circle = new Circle(cx, cy, r);
+    circle.setColor(color);
+    circle.setLetters(letters);
+    return circle.render();
+  } else if (shape === 'Square') {
+    const square = new Square(cx, cy, r);
+    square.setColor(color);
+    square.setLetters(letters);
+    return square.render();
+  } else if (shape === 'Triangle') {
+    const triangle = new Triangle(cx, cy, r);
+    triangle.setColor(color);
+    triangle.setLetters(letters);
+    return triangle.render();
+  }
+};
 
 inquirer
-.prompt([
+  .prompt([
     {
-        type: 'input',
-        name: 'text',
-        message: 'Enter three characters',
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape',
-        choices: ["Square", "Triangle", "Circle"],
-      },
-      {
-        type: 'input',
-        name: 'color',
-        message: 'Choose a color (any!)',
-      },
+      type: 'input',
+      name: 'letters',
+      message: 'Enter three characters',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape',
+      choices: ['Square', 'Triangle', 'Circle'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Choose a color (any!)',
+    },
+    {
+      type: 'input',
+      name: 'cx',
+      message: 'Enter the cx value',
+    },
+    {
+      type: 'input',
+      name: 'cy',
+      message: 'Enter the cy value',
+    },
+    {
+      type: 'input',
+      name: 'r',
+      message: 'Enter the r value',
+    },
+  ])
+  .then((answers) => {
+    const svgImageContent = generateSVG(answers);
 
-])
+    fs.writeFile('logo.svg', svgImageContent, (err) =>
+      err ? console.log(err) : console.log('Generated logo.svg')
+    );
+  });
